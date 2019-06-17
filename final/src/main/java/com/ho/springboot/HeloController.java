2f27e92a8e4a2f8ff173073eb20bc2964b140f6b
ysh3718 <yof3718@naver.com>
2019-06-17 19:36:27 +0900

    HeloController_delete_remove_update

--- final/src/main/java/com/ho/springboot/HeloController.java
+++ final/src/main/java/com/ho/springboot/HeloController.java
@@ -77,4 +77,10 @@ public class HeloController {
 		mav.addObject("datalist", list);
 		return mav;
 	}
+	
+	@RequestMapping(value = "/delete", method = RequestMethod.POST)
+	public ModelAndView remove(@RequestParam("id") String id, ModelAndView mav) {
+		repository.deleteById(id);
+		return new ModelAndView("redirect:/");
+	}
 }
