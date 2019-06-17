d41a1d9dd8eab2b8c3b89fccf56b432b62499246
ysh3718 <yof3718@naver.com>
2019-06-17 19:16:52 +0900

    HeloController_index_requestmapping_update

--- final/src/main/java/com/ho/springboot/HeloController.java
+++ final/src/main/java/com/ho/springboot/HeloController.java
@@ -1,5 +1,35 @@
 package com.ho.springboot;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ho.springboot.MyDataMongo;
+import com.ho.springboot.repositories.MyDataMongoRepository;
+
+@Controller
 public class HeloController {
+
+	@Autowired
+	MyDataMongoRepository repository;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav) {
+
+		mav.setViewName("index");
+		mav.addObject("title", "기초대사량 계산기");
+		mav.addObject("msg", "스펙을 입력해주세요.");
+
+		Iterable<MyDataMongo> list = repository.findAll();
+		mav.addObject("datalist", list);
+		return mav;
+	}
 
 }
