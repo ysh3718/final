28a750aeb40ce3ec5a3cb888a068954558ac519c
ysh3718 <yof3718@naver.com>
2019-06-17 18:54:58 +0900

    MyDataMongoRepository_update

--- final/src/main/java/com/ho/springboot/repositories/MyDataMongoRepository.java
+++ final/src/main/java/com/ho/springboot/repositories/MyDataMongoRepository.java
@@ -1,5 +1,13 @@
 package com.ho.springboot.repositories;
 
-public class MyDataMongoRepository {
+import java.util.List;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.ho.springboot.MyDataMongo;
+
+public interface MyDataMongoRepository extends MongoRepository<MyDataMongo, Long> 
+{
+	public List<MyDataMongo> findById(String id);
+	public void deleteById(String param);
 }
